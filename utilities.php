13f1f6c6a265155af61a78fa07b3200636dd5ce0
Luke Swanson <lwswanson2@wisc.edu>
2023-10-05 12:36:59 -0500

    Add utilities file containing functions for sending data to event monitor.

---
+++ utilities.php
@@ -0,0 +1,60 @@
+<?php
+
+// connect to flask app with flask api url
+// send json package to flask app
+function connectFlask($AppID, $jsonPackage)
+{
+    include('config.php');
+    $flaskApiUrl = $flaskURL . $AppID;
+    $timeout = 0.1;
+    $ch = curl_init($flaskApiUrl);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt(
+        $ch,
+        CURLOPT_HTTPHEADER,
+        array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($jsonPackage)
+        )
+    );
+
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonPackage);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 50); // DEPLOYMENT CHANGE
+    curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1); //timeout in seconds
+    $response = curl_exec($ch);
+
+    // check for cURL errors
+    if (curl_errno($ch)) {
+        echo 'cURL error: ' . curl_error($ch);
+    } else {
+        echo 'Response from flask app: ' . $response;
+    }
+
+    // close cURL session
+    curl_close($ch);
+}
+
+// given <parameter array from $_REQUEST> AND <body object from $data>
+// iterate these two and return a combined <jsonPackage> including each item of both
+function combineParaAndBody($paraArray, $bodyObject)
+{
+    $jsonPackage = array();
+
+    foreach ($paraArray as $key => $value) {
+        $jsonPackage[$key] = $value;
+    }
+
+    foreach ($bodyObject as $key => $value) {
+        $jsonPackage[$key] = $value;
+    }
+
+    // remove the elements you do not want to send through log.php to flaskapp
+    unset($jsonPackage["data"]); // contains long string of encoded data
+    unset($jsonPackage["remote_addr"]);
+    unset($jsonPackage["http_user_agent"]);
+
+    return json_encode($jsonPackage);
+}
+?>
