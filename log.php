b13e97569e3d83e5b0e1278b2c81dd1bf09c45d1
Luke Swanson <lwswanson2@wisc.edu>
2024-02-09 13:04:05 -0600

    Some minor renamings and cleanups before testing this on server.

--- log.php
+++ log.php
@@ -7,18 +7,18 @@ include('query_generator.php');
 include('utilities.php');
 # 1. Figure out what the input schema looks like
 $REQUEST_SCHEMA = $OGD_SCHEMA;
-$UPPER = "NO APP ID";
+$APP_ID = "NO APP ID";
 if (isset($_REQUEST["app_id"])) {
-  $UPPER = strtoupper($_REQUEST["app_id"]);
+  $APP_ID = strtoupper($_REQUEST["app_id"]);
   // error_log("The app id in upper-case is: ".$upper);
   $logger_games = array("BACTERIA",   "BALLOON",  "CRYSTAL",    "CYCLE_CARBON", "CYCLE_NITROGEN", "CYCLE_WATER",
                         "EARTHQUAKE", "JOWILDER", "LAKELAND",   "MAGNET",       "WAVES",          "WIND");
   $ogd_games    = array("AQUALAB",    "ICECUBE", "JOURNALISM", "MASHOPOLIS", "PENGUINS", "THERMOVR",
                         "TRANSFORMATION_QUEST");
-  if (in_array($UPPER, $logger_games)) {
+  if (in_array($APP_ID, $logger_games)) {
     $REQUEST_SCHEMA = $LOGGER_SCHEMA;
   }
-  elseif (in_array($UPPER, $ogd_games)) {
+  elseif (in_array($APP_ID, $ogd_games)) {
     $REQUEST_SCHEMA = $OGD_SCHEMA;
   }
 }
@@ -39,34 +39,21 @@ if (!is_array($data)) {
 
 # 4. Send the query itself. Log errors if failed.
 if (count($data) > 0) {
-  $query = generateQueryString($REQUEST_SCHEMA, $UPPER, $data, $conn);
+  $query = generateQueryString($REQUEST_SCHEMA, $APP_ID, $data, $conn);
   $result = mysqli_query($conn, $query);
   if ($result) {
-    # 5. Make flask connection after connecting to db
-    $loggerData = combineParaAndBody($_REQUEST, $data[0]);
+# 5. Make flask monitor connection after connecting to db
+    $loggerData = combineParamsAndBody($_REQUEST, $data[0]);
     // $current_time = microtime(true);
     // $current_time_milliseconds = round($current_time * 1000);
     // error_log("beforeTime:".$current_time_milliseconds);
-    connectFlask("all-game", $loggerData);
-    if ($REQUEST_SCHEMA == $OGD_SCHEMA) {
-      // $loggerData = combineParaAndBody($_REQUEST, $data[0]);
-      // // $current_time = microtime(true);
-      // // $current_time_milliseconds = round($current_time * 1000);
-      // // error_log("beforeTime:".$current_time_milliseconds);
-      // connectFlask("all-game", $loggerData);
-      // // $next_time = microtime(true);
-      // // $next_time_milliseconds = round($next_time * 1000);
-      // // error_log("afterTime:".$next_time_milliseconds);
-
-      // // error_log("timeDiff".($next_time_milliseconds-$current_time_milliseconds)/1000);
-      // // connectFlask($_REQUEST["app-id"], $loggerData);
-    } else {
-      error_log("Warning: Got an old-logger data format");
-    }
+    sendToMonitor($loggerData);
+    // if ($REQUEST_SCHEMA != $OGD_SCHEMA) {
+    //   error_log("Warning: Got an old-logger data format");
+    // }
     // error_log("log.php test flaskApiUrl and loggerData \nflaskApiUrl: " . $flaskApiUrl . "\nloggerData: " . $loggerData);
   } else {
-    $sql_err = "Query for " . $UPPER . " failed with error: " . mysqli_error($conn);
-    // error_log("The query is: ".$query);
+    $sql_err = "Query for " . $APP_ID . " failed with error: " . mysqli_error($conn);
     error_log($sql_err);
     die("FAIL: ".$sql_err);
   }
--- utilities.php
+++ utilities.php
@@ -2,28 +2,23 @@
 
 // connect to flask app with flask api url
 // send json package to flask app
-function connectFlask($AppID, $jsonPackage)
+function sendToMonitor($jsonPackage)
 {
     include('config.php');
-    $flaskApiUrl = $flaskURL . $AppID;
-    $timeout = 0.1;
-    $ch = curl_init($flaskApiUrl);
-    curl_setopt($ch, CURLOPT_PORT, 5022);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($jsonPackage)
-        )
+    $ch = curl_init($flaskApiUrl . '/log/event');
+    $headers = array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($jsonPackage)
     );
-
-    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_PORT, 5022);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonPackage);
+    // $timeout = 0.1;
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 50); // DEPLOYMENT CHANGE
-    curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1); //timeout in seconds
+    curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1);
     $response = curl_exec($ch);
 
     // check for cURL errors
@@ -40,23 +35,23 @@ function connectFlask($AppID, $jsonPackage)
 
 // given <parameter array from $_REQUEST> AND <body object from $data>
 // iterate these two and return a combined <jsonPackage> including each item of both
-function combineParaAndBody($paraArray, $bodyObject)
+function combineParamsAndBody($paramArray, $bodyObject)
 {
-    $jsonPackage = array();
+    $ret_val = array();
 
-    foreach ($paraArray as $key => $value) {
-        $jsonPackage[$key] = $value;
+    foreach ($paramArray as $key => $value) {
+        $ret_val[$key] = $value;
     }
 
     foreach ($bodyObject as $key => $value) {
-        $jsonPackage[$key] = $value;
+        $ret_val[$key] = $value;
     }
 
     // remove the elements you do not want to send through log.php to flaskapp
-    unset($jsonPackage["data"]); // contains long string of encoded data
-    unset($jsonPackage["remote_addr"]);
-    unset($jsonPackage["http_user_agent"]);
+    unset($ret_val["data"]); // contains long string of encoded data
+    unset($ret_val["remote_addr"]);
+    unset($ret_val["http_user_agent"]);
 
-    return json_encode($jsonPackage);
+    return json_encode($ret_val);
 }
 ?>
