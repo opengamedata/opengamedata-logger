c01dea9365c935e46fd133a6e6f4c7817b03f461
Luke Swanson <lwswanson2@wisc.edu>
2024-02-12 15:13:53 -0600

    Include test error_log calls to see if there's differences in what gets logged where.

--- log.php
+++ log.php
@@ -47,9 +47,11 @@ if (count($data) > 0) {
       $loggerData = combineParamsAndBody($_REQUEST, $data[0]);
       $start_time_milliseconds = round(microtime(true) * 1000);
       syslog(LOG_NOTICE, "Sending data to monitor, beforeTime:".$start_time_milliseconds);
+      error_log("Repeat message with error_log: Sending data to monitor, beforeTime:".$start_time_milliseconds);
       sendToMonitor($loggerData);
       $end_time_milliseconds = round(microtime(true) * 1000);
       syslog(LOG_NOTICE, "Sent data to monitor, timedelta:".($end_time_milliseconds - $start_time_milliseconds));
+      error_log("Repeat message with error_log: Sent data to monitor, timedelta:".($end_time_milliseconds - $start_time_milliseconds));
       // if ($REQUEST_SCHEMA != $OGD_SCHEMA) {
       //   syslog(LOG_WARNING, "Warning: Got an old-logger data format");
       // }
